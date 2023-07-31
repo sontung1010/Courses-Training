fe1f4d2e0300be391906eb1f6ea63472fc4578d2
sontung1010 <sontungcodedao@gmail.com>
2023-07-30 23:49:35 -0400

    update

--- Day_20_21/scoreboard.py
+++ Day_20_21/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 12, "normal")
 
 
 
@@ -8,12 +10,16 @@ class Scoreboard(Turtle):
         self.score = 0
         self.penup()
         self.color("white")
-        self.goto(0, 270)
+        self.goto(0, 240)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
